00a63522fcda1801d60e7aee9ebe88ef1f9a5342
wahyu1709 <44396432+wahyu1709@users.noreply.github.com>
2021-03-21 23:47:11 +0700

    Add files via upload

---
+++ form_nilai.php
@@ -0,0 +1,110 @@
+<html>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+	<div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="well well-sm">
+          <form class="form-horizontal" action="form_nilai.php" method="post">
+          <fieldset>
+            <legend class="text-center">Form nilai siswa</legend>
+    
+            <!-- Name input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="name">Nama siswa</label>
+              <div class="col-md-9">
+                <input id="name" name="nama" type="text" placeholder="Nama Siswa" class="form-control">
+              </div>
+            </div>
+    
+            <!-- mata kuliah input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="">Mata kuliah</label>
+              <div class="col-md-9">
+                <input id="" name="matkul" type="text" placeholder="mata kuliah" class="form-control">
+              </div>
+            </div>
+
+            <!-- mata kuliah input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="">Nilai UTS</label>
+              <div class="col-md-9">
+                <input id="" name="nilai_uts" type="text" placeholder="Nilai UTS" class="form-control">
+              </div>
+            </div>
+
+            <!-- mata kuliah input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="">Nilai UAS</label>
+              <div class="col-md-9">
+                <input id="" name="nilai_uas" type="text" placeholder="Nilai UAS" class="form-control">
+              </div>
+            </div>
+
+            <!-- mata kuliah input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="">Nilai Tugas Praktikum</label>
+              <div class="col-md-9">
+                <input id="" name="nilai_tugas" type="text" placeholder="NIlai Tugas" class="form-control">
+              </div>
+            </div>
+    
+            
+    
+            <!-- Form actions -->
+            <div class="form-group">
+              <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+              </div>
+            </div>
+          </fieldset>
+          </form>
+        </div>
+      </div>
+	</div>
+</div>
+</html>
+<?php
+$proses= $_POST['proses'];
+$nama_siswa = $_POST['nama'];
+$mata_kuliah = $_POST['matkul'];
+$nilai_uts = $_POST['nilai_uts'];
+$nilai_uas = $_POST['nilai_uas'];
+$nilai_tugas = $_POST['nilai_tugas'];
+$nilai_total = ($nilai_uts*30/100)+($nilai_uas*35/100)+($nilai_tugas*35/100);
+if(!empty($proses)) {
+    echo 'Proses : '.$proses;
+    echo '<br/>Nama : '.$nama_siswa;
+    echo '<br/>Mata Kuliah : '.$mata_kuliah;
+    echo '<br/>Nilai UTS : '.$nilai_uts;  
+    echo '<br/>Nilai UAS : '.$nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : '.$nilai_tugas;
+    
+      if($nilai_total >= 85){
+        echo '<br/>Anda lulus';
+        echo '<br/>Grade nilai anda A';
+      }
+      elseif($nilai_total >= 70){
+        echo '<br/>Anda lulus';
+        echo '<br/>Grade nilai anda B';
+      }
+      elseif($nilai_total >= 56){
+        echo '<br/>Anda lulus';
+        echo '<br/>Grade nilai anda C';
+      }
+      elseif($nilai_total >= 36){
+        echo '<br/>Anda tidak lulus';
+        echo '<br/>Grade nilai anda D';
+      }
+      elseif($nilai_total >= 0){
+        echo '<br/>Anda tidak lulus';
+        echo '<br/>Grade nilai anda E';
+      }
+      else{
+        echo '<br/>Anda tidak lulus';
+      }
+    }
+    ?>
