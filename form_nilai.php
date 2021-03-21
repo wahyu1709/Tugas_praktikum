834bf43bebbe5f5313c75d0a1329652e77809462
wahyu1709 <44396432+wahyu1709@users.noreply.github.com>
2021-03-22 00:21:07 +0700

    Add files via upload

--- form_nilai.php
+++ form_nilai.php
@@ -8,47 +8,47 @@
 	<div class="row">
       <div class="col-md-6 col-md-offset-3">
         <div class="well well-sm">
-          <form class="form-horizontal" action="form_nilai.php" method="post">
+          <form name="proses" class="form-horizontal" action="form_nilai.php" method="post">
           <fieldset>
             <legend class="text-center">Form nilai siswa</legend>
     
             <!-- Name input-->
             <div class="form-group">
-              <label class="col-md-3 control-label" for="name">Nama siswa</label>
+              <label class="col-md-3 control-label" for="nama">Nama siswa</label>
               <div class="col-md-9">
-                <input id="name" name="nama" type="text" placeholder="Nama Siswa" class="form-control">
+                <input id="nama" name="nama" type="text" placeholder="Nama Siswa" class="form-control">
               </div>
             </div>
     
             <!-- mata kuliah input-->
             <div class="form-group">
-              <label class="col-md-3 control-label" for="">Mata kuliah</label>
+              <label class="col-md-3 control-label" for="matkul">Mata kuliah</label>
               <div class="col-md-9">
-                <input id="" name="matkul" type="text" placeholder="mata kuliah" class="form-control">
+                <input id="matkul" name="matkul" type="text" placeholder="mata kuliah" class="form-control">
               </div>
             </div>
 
             <!-- mata kuliah input-->
             <div class="form-group">
-              <label class="col-md-3 control-label" for="">Nilai UTS</label>
+              <label class="col-md-3 control-label" for="nilai_uts">Nilai UTS</label>
               <div class="col-md-9">
-                <input id="" name="nilai_uts" type="text" placeholder="Nilai UTS" class="form-control">
+                <input id="nilai_uts" name="nilai_uts" type="text" placeholder="Nilai UTS" class="form-control">
               </div>
             </div>
 
             <!-- mata kuliah input-->
             <div class="form-group">
-              <label class="col-md-3 control-label" for="">Nilai UAS</label>
+              <label class="col-md-3 control-label" for="nilai_uas">Nilai UAS</label>
               <div class="col-md-9">
-                <input id="" name="nilai_uas" type="text" placeholder="Nilai UAS" class="form-control">
+                <input id="nilai_uas" name="nilai_uas" type="text" placeholder="Nilai UAS" class="form-control">
               </div>
             </div>
 
             <!-- mata kuliah input-->
             <div class="form-group">
-              <label class="col-md-3 control-label" for="">Nilai Tugas Praktikum</label>
+              <label class="col-md-3 control-label" for="nilai_tugas">Nilai Tugas Praktikum</label>
               <div class="col-md-9">
-                <input id="" name="nilai_tugas" type="text" placeholder="NIlai Tugas" class="form-control">
+                <input id="nilai_tugas" name="nilai_tugas" type="text" placeholder="NIlai Tugas" class="form-control">
               </div>
             </div>
     
@@ -75,7 +75,7 @@ $nilai_uts = $_POST['nilai_uts'];
 $nilai_uas = $_POST['nilai_uas'];
 $nilai_tugas = $_POST['nilai_tugas'];
 $nilai_total = ($nilai_uts*30/100)+($nilai_uas*35/100)+($nilai_tugas*35/100);
-if(!empty($proses)) {
+
     echo 'Proses : '.$proses;
     echo '<br/>Nama : '.$nama_siswa;
     echo '<br/>Mata Kuliah : '.$mata_kuliah;
@@ -106,5 +106,5 @@ if(!empty($proses)) {
       else{
         echo '<br/>Anda tidak lulus';
       }
-    }
+    
     ?>
